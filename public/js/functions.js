631b41c15e702e8d10bc98e7c1813e0a65117ce2
Laurent <laurent.marquet@laposte.net>
2021-06-24 10:53:53 +0200

    - Removed used of 410 errors emails (24/06/2021)

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,8 +1,8 @@
 /* javascript for sandbox.975l.com */
 
 //Defines current filePath
-var filePath = $('script[src*=functions]').attr('src');
-filePath = filePath.replace('functions.min.js', '');
+var filePath = $("script[src*=functions]").attr("src");
+filePath = filePath.replace("functions.min.js", "");
 
 $(document).ready(function() {
 });
